33e1cdac4238cee63bee466d425e2fbf8ff9dbc7
DoriTran <tranquocdong308@gmail.com>
2025-06-27 12:47:20 +0700

    done positioning new navigate btn

---
+++ src/components/Outlet/NavigateBtn/NavigationBtn.js
@@ -0,0 +1,103 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useMemo, useState } from "react";
+import clsx from "clsx";
+import ApIcon from "components/ApIcon/ApIcon";
+import { faBagShopping, faBookBookmark, faSeedling, faTree } from "@fortawesome/free-solid-svg-icons";
+import styles from "./NavigationBtn.module.scss";
+
+const icons = { grow: faSeedling, garden: faTree, shop: faBagShopping, album: faBookBookmark };
+const colors = { grow: "var(--grow)", garden: "var(--garden)", shop: "var(--shop)", album: "var(--album)" };
+const navKeys = ["grow", "garden", "shop", "album"];
+const defaultNavPos = { top: 35, left: 35 };
+const radius = 70;
+
+const NavigationBtn = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // Icon state
+  const [front, setFront] = useState({ icon: null, color: "" });
+  const [back, setBack] = useState({ icon: null, color: "" });
+
+  // Flip state
+  const [nextLoc, setNextLoc] = useState(location.pathname);
+  const [isFlipping, setIsFlipping] = useState(false);
+
+  useEffect(() => {
+    // First time load page
+    if (front.icon === null) {
+      const loc = location.pathname.split("/")[1];
+      setNextLoc(loc);
+      setFront({ icon: icons[loc], color: colors[loc] });
+      return;
+    }
+
+    // On location change
+    setIsFlipping(true);
+    setBack({ icon: icons[nextLoc], color: colors[nextLoc] });
+    navigate(`/${nextLoc}`);
+
+    // After flipping - reverse faces
+    const timer = setTimeout(() => {
+      setFront(back);
+      setIsFlipping(false);
+    }, 600);
+
+    return () => clearTimeout(timer);
+  }, [nextLoc]);
+
+  // Open navigation
+  const [isOpen, setIsOpen] = useState(false);
+  const smallNavPosition = useMemo(() => {
+    // Unopen case
+    if (!isOpen) return Object.fromEntries(navKeys.map((key) => [key, { ...defaultNavPos }]));
+
+    // Open case
+    const startAngle = 0; // 0 right
+    const endAngle = Math.PI / 2; // 90 down
+
+    const keysToPlace = navKeys.filter((key) => key !== nextLoc);
+    const totalPoints = keysToPlace.length;
+    const angleStep = (endAngle - startAngle) / (totalPoints - 1);
+
+    return Object.fromEntries(
+      navKeys.map((key) => {
+        if (key === nextLoc) {
+          return [key, { ...defaultNavPos }];
+        }
+
+        const index = keysToPlace.indexOf(key);
+        const angle = startAngle - index * angleStep;
+        const x = Math.cos(angle) * radius;
+        const y = -Math.sin(angle) * radius;
+
+        return [key, { left: x + 35, top: y + 35 }];
+      })
+    );
+  }, [isOpen, nextLoc]);
+
+  return (
+    <div className={clsx(styles.container, { [styles.flipped]: isFlipping })} onClick={() => setIsOpen(!isOpen)}>
+      <div className={styles.inner}>
+        <div className={clsx(styles.face, styles.front)} style={{ borderColor: front.color }}>
+          <ApIcon icon={front.icon || faSeedling} color={front.color} size={28} />
+        </div>
+        <div className={clsx(styles.face, styles.back)} style={{ borderColor: back.color }}>
+          <ApIcon icon={back.icon || faSeedling} color={back.color} size={28} />
+        </div>
+      </div>
+      {navKeys.map((nav) => (
+        <div
+          key={nav}
+          className={styles.nav}
+          style={{ left: smallNavPosition[nav].left, top: smallNavPosition[nav].top, backgroundColor: colors[nav] }}
+          onClick={() => setNextLoc(nav)}
+        >
+          <ApIcon icon={icons[nav]} color="white" size={20} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NavigationBtn;
--- src/components/Outlet/Outlet.js
+++ src/components/Outlet/Outlet.js
@@ -1,12 +1,13 @@
 import { Outlet as DomLibOutlet, useNavigate } from "react-router-dom";
 import GardenButton from "./GardenButton/GardenButton";
+import NavigationBtn from "./NavigateBtn/NavigationBtn";
 import styles from "./Outlet.module.scss";
 
 const Outlet = () => {
   const navigate = useNavigate();
   return (
     <main className={styles.main}>
-      <GardenButton />
+      <NavigationBtn />
       <DomLibOutlet />
     </main>
   );
